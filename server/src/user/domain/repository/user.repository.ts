480a9006db1f05457139e6088df37ae3ffb243e2
Kangila404 <ia3264666@gmail.com>
2026-06-29 21:15:12 +0900

    fix(user) : lint 에러 방지용 repository 내용 채우기

--- server/src/user/domain/repository/user.repository.ts
+++ server/src/user/domain/repository/user.repository.ts
@@ -3,5 +3,5 @@ import { User } from '../model/user.entity';
 export const USER_REPOSITORY = Symbol('USER_REPOSITORY');
 
 export interface UserRepository {
-
+    findById(id:string): Promise<User | null>;
 }
